f603418919308214144da54acb36ebbc001634e4
Assanalikhan <210103042@stu.sdu.edu.kz>
2023-12-13 23:20:41 +0600

    eight

---
+++ basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/test/java/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,15 @@
+package fake
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import kotlinx.coroutines.test.runTest
+
+@Test
+fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+    runTest {
+        val repository = NetworkMarsPhotosRepository(
+            marsApiService = FakeMarsApiService()
+        )
+        assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+    }
